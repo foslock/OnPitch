9e8a8d2e2b9ccad0eb0afda17f882bd48d859b34
Dan Fortunato <dan.fortunato@gmail.com>
2013-03-03 21:41:38 -0500

    Added empty OPSong class

---
+++ On Pitch/On Pitch/OPSong.h
@@ -0,0 +1,13 @@
+//
+//  OPSong.h
+//  On Pitch
+//
+//  Created by Dan Fortunato on 2/26/13.
+//  Copyright (c) 2013 Tufts Dev Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OPSong : NSObject
+
+@end
---
+++ On Pitch/On Pitch/OPSong.m
@@ -0,0 +1,13 @@
+//
+//  OPSong.m
+//  On Pitch
+//
+//  Created by Dan Fortunato on 2/26/13.
+//  Copyright (c) 2013 Tufts Dev Team. All rights reserved.
+//
+
+#import "OPSong.h"
+
+@implementation OPSong
+
+@end
