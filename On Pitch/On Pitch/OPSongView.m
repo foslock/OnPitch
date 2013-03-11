a3ce8a47ddcc6a6a99bb5d87653ba6fefdb46a32
Dan Fortunato <dan.fortunato@gmail.com>
2013-03-11 16:26:13 -0400

    Drawing the staff

--- On Pitch/On Pitch/OPSongView.m
+++ On Pitch/On Pitch/OPSongView.m
@@ -13,6 +13,9 @@
 #define NOTE_HEIGHT 10.0f
 #define NOTE_SPACING 10.0f
 #define REST_HEIGHT 20.0f
+#define STAFF_LINEWIDTH 5.0f
+#define NUMBER_OF_STAFF_LINES 5
+#define LINE_SPACING 10.0f
 
 @interface OPSongView ()
 
@@ -44,21 +47,39 @@
     // Clear the space
     CGContextClearRect(context, rect);
     
+    // Draw staff
+    CGContextSetLineWidth(context, STAFF_LINEWIDTH);
+    CGContextSetStrokeColorWithColor(context, [UIColor blackColor].CGColor);
+    for (NSInteger i=0; i<NUMBER_OF_STAFF_LINES; i++) {
+        CGContextMoveToPoint(context, 0.0f, i * LINE_SPACING);
+        CGContextAddLineToPoint(context, 0.0f, i * LINE_SPACING);
+        CGContextStrokePath(context);
+    }
+    
+    // Draw notes
     for (NSInteger i=0; i<self.song.notes.count; i++)
     {
         OPNote *n = [self.song.notes objectAtIndex:i];
         CGFloat width = (CGFloat)n.length;
-        CGFloat x = 
+        CGFloat x = (CGFloat)n.timestamp;
         CGFloat y = REST_HEIGHT;
         if (n.nameIndex != kNoteNameNone) {
-            CGFloat y = n.noteIndex * NOTE_SPACING;
+            y = n.noteIndex * NOTE_SPACING;
         }
         
+        // Maybe change the color depending on the note?
         CGContextSetFillColorWithColor(context, [UIColor blueColor].CGColor);
         CGRect noteRect = CGRectMake(x, y, width, NOTE_HEIGHT);
-        CGContextFillRect(context, noteRect)
-        CGContextStrokePath(currentContext);
+        CGContextFillRect(context, noteRect);
+        CGContextStrokePath(context);
+    }
+}
 
+- (CGColorRef)colorForNote:(OPNote *)note
+{
+    NSInteger i = note.noteIndex;
+    UIColor *color = [UIColor colorWithRed:(CGFloat)i green:(CGFloat)i blue:(CGFloat)i alpha:(CGFloat)i];
+    return color.CGColor;
 }
 
 @end
