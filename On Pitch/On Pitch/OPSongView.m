5437c150d41df4fed19be4f90194f86681145ea3
Dan Fortunato <dan.fortunato@gmail.com>
2013-03-11 17:13:25 -0400

    Testing MIDI rendering

--- On Pitch/On Pitch/OPSongView.m
+++ On Pitch/On Pitch/OPSongView.m
@@ -11,12 +11,14 @@
 #import "OPFeedbackView.h"
 
 // Change these 'til it looks purty
-#define NOTE_HEIGHT 10.0f
+#define NOTE_HEIGHT 2.0f
 #define NOTE_SPACING 10.0f
+#define NOTE_LENGTH_SCALE_FACTOR 100.0f
 #define REST_HEIGHT 20.0f
-#define STAFF_LINEWIDTH 5.0f
+#define STAFF_LINEWIDTH 3.0f
 #define NUMBER_OF_STAFF_LINES 5
-#define LINE_SPACING 10.0f
+#define LINE_SPACING 70.0f
+#define STAFF_OFFSET 20.0f
 
 @interface OPSongView ()
 
@@ -37,6 +39,10 @@
     self.contentWidth = self.bounds.size.width;
     self.panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(viewDidPan:)];
     [self addGestureRecognizer:self.panGesture];
+    
+    NSString *testPath = [[NSBundle mainBundle] pathForResource:@"santa" ofType:@"mid"];
+    OPSong *s = [[OPSong alloc] initWithMIDIFile:testPath];
+    self.song = s;
 }
 
 - (id)initWithSong:(OPSong *)s
@@ -96,17 +102,20 @@
     CGContextSetLineWidth(context, STAFF_LINEWIDTH);
     CGContextSetStrokeColorWithColor(context, [UIColor blackColor].CGColor);
     for (NSInteger i=0; i<NUMBER_OF_STAFF_LINES; i++) {
-        CGContextMoveToPoint(context, 0.0f, i * LINE_SPACING);
-        CGContextAddLineToPoint(context, 0.0f, i * LINE_SPACING);
+        CGContextMoveToPoint(context, 0.0f, self.bounds.size.height/2.0f+(i-2)*LINE_SPACING);
+        CGContextAddLineToPoint(context, self.bounds.size.width,
+                                self.bounds.size.height/2.0f+(i-2)*LINE_SPACING);
         CGContextStrokePath(context);
     }
     
     // Draw notes
+    
     for (NSInteger i=0; i<self.song.notes.count; i++)
     {
         OPNote *n = [self.song.notes objectAtIndex:i];
-        CGFloat width = (CGFloat)n.length;
+        CGFloat width = (CGFloat)n.length * NOTE_LENGTH_SCALE_FACTOR;
         CGFloat x = (CGFloat)n.timestamp;
+        NSLog(@"%f", x);
         CGFloat y = REST_HEIGHT;
         if (n.nameIndex != kNoteNameNone) {
             y = n.noteIndex * NOTE_SPACING;
@@ -118,6 +127,7 @@
         CGContextFillRect(context, noteRect);
         CGContextStrokePath(context);
     }
+    
 }
 
 - (CGColorRef)colorForNote:(OPNote *)note
